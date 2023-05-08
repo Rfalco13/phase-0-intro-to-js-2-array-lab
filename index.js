0ae4ffe9eebe70df38d9327e15a6e0bf76d9ebb4
ryanfalco13@gmail.com <ryanfalco13@gmail.com>
2023-05-08 15:19:47 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(Bob) {
+    cats.push(Bob);
+  }
+
+  function destructivelyPrependCat(Bob) {
+    cats.unshift(Bob);
+  }
+  function destructivelyRemoveLastCat() {
+    cats.pop();
+  }
+  
+  function destructivelyRemoveFirstCat() {
+    cats.shift();
+    return cats;
+  }
+
+  function appendCat(Milo) {
+    return [...cats, Milo];
+  }
+  function prependCat(Otis) {
+    return [Otis, ...cats];
+  }
+  function removeLastCat() {
+    
+    let newCats = cats.slice();
+    
+    newCats.pop();
+    
+    return newCats;
+  }
+  function removeFirstCat() {
+    return cats.slice(1);
+  }
